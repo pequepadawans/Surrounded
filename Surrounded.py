573b89945669b66d2f720c67506502f414ac2f55
pequepadawans <pequepadawans@gmail.com>
2025-12-24 18:29:50 +0100

    Adjust ARP request timeout and retry settings
    Increased timeout and added retry for ARP requests.

--- Surrounded.py
+++ Surrounded.py
@@ -17,7 +17,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=3, retry=2, verbose=False)[0]
 
     # store as list
     clients_list = []
